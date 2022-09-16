50bdf7fdf0cbaedd680cb1869db276121ebfade6
Recruit_Zer0 <tadeudosgamesOFICIAL@gmail.com>
2022-09-16 20:28:21 -0300

    update

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1663342939,
+	"version": 1663370803,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -8,12 +8,6 @@
 		"images/background.png",
 		"images/player-sheet0.png",
 		"images/player-sheet1.png",
-		"images/espinhos-sheet0.png",
-		"icon-16.png",
-		"icon-32.png",
-		"icon-114.png",
-		"icon-128.png",
-		"icon-256.png",
-		"loading-logo.png"
+		"images/espinhos-sheet0.png"
 	]
 }
